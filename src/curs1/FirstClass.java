8274ceb69492ecf81bcaf92fc0441fc822e49c5d
Sorana L <alexandra.badarau@yahoo.com>
2023-02-23 21:43:44 +0200

    curs2

--- src/curs1/FirstClass.java
+++ src/curs1/FirstClass.java
@@ -8,12 +8,31 @@ package curs1;
  */
 
 public class FirstClass {
+	
+	//este o variabila de instanta -> are nevoie de un obiect al clasei pt care este utilizata
+	String nume = "Ioana";
+	int varsta = 25;
+	
+	
 
 	//metoda main- metoda principala de executie
 	public static void main(String[] args) {
 
 		System.out.println("buna seara");
+		
+		FirstClass obiect = new FirstClass();
+		
 
+		//este o variabila locala
+		String prenume = "Ana";
+
+
+		System.out.println(prenume);
+
+		System.out.println(obiect.nume);
+		
+		
+		
 	}
 
 }
---
+++ src/curs2/Tester.java
@@ -0,0 +1,38 @@
+package curs2;
+
+public class Tester {
+
+	//constructor default
+	public Tester() {
+		
+	}
+	
+	
+	//variabile
+	String name;
+	int ratePerHour;
+	
+	
+	//metode
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getRatePerHour() {
+		return ratePerHour;
+	}
+	
+	public void setRatePerHour(int ratePerHour) {
+		this.ratePerHour = ratePerHour;
+	}
+	
+	
+	
+	
+	
+	
+}
