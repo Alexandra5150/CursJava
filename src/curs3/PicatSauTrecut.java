8af35471b3daceeb05efafd6a95c9fbbaa90df02
Sorana L <alexandra.badarau@yahoo.com>
2023-03-01 12:38:10 +0200

    completare tema curs 3

--- src/curs3/PicatSauTrecut.java
+++ src/curs3/PicatSauTrecut.java
@@ -4,10 +4,10 @@ public class PicatSauTrecut {
 	
 	public void verificaPicatSauTrecut (int numar) {
 		
-		if(numar < 65) {
+		if(numar >= 0 && numar <= 65) {
 			System.out.println("Ai picat. Mai incearca.");
 			
-		} else {
+		} else if (numar >= 66) {
 			System.out.println("Felicitari, ai trecut.");
 			
 		}
