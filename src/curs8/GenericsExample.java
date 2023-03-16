98897f5096b9ed2bbcb7467dc13692822be21c7f
Sorana L <alexandra.badarau@yahoo.com>
2023-03-16 21:35:26 +0200

    curs8

---
+++ src/curs8/GenericsExample.java
@@ -0,0 +1,37 @@
+package curs8;
+
+public class GenericsExample {
+
+	public static void main(String[] args) {
+		
+		GenericsExample obj = new GenericsExample();
+		obj.printDetails("text");
+		obj.printDetails(123);
+		obj.printDetails('c');
+		obj.printDetails(12.35);
+
+	}
+
+	
+	public <T> void printDetails(T obj) {
+		
+		System.out.println("Obiectul meu este: " + obj);
+		
+	}
+	
+	
+//	public void printDetails(String text) {
+//		
+//		System.out.println("Obiectul meu este: " + text);
+//		
+//	}
+	
+//	public void printDetails(int nr) {
+//		
+//		System.out.println("Obiectul meu este: " + nr);
+//		
+//	}
+	
+	
+	
+}
